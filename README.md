200c75f776662e4d914eb343213d0368dcd28cfa
argenestel <arpitpali68@gmail.com>
2024-08-28 15:53:14 +0530

    readme

--- README.md
+++ README.md
@@ -1,39 +1,16 @@
-# ZeekCraft
+# BNBCraft
 
-Endless Fully Onchain Crafting Game On zksync
+Endless Fully Onchain Crafting Game On opBNB
 
 
 ## Contract
 
-Deployed on Zk sepolia Testnet
+Deployed on opBNB Testnet
 
 
 1. Main Crafting Contract:
 
-    ZeekCraft: "0xF9dE10bCB84901b67a19d99F535989521B8d03FB",
+    BNBCraft: "0xE534b74346a3759A094619cbf3a9D02B6E3DEF75",
     
 
-    Explorer: https://sepolia.explorer.zksync.io/address/0xF9dE10bCB84901b67a19d99F535989521B8d03FB
-    
 
-2. Zeek Token Contract:
-
-ZeekCraftToken: "0x794CdABc17830f4bcaAACa7ee1658c31942845dA",
-
-
-3. Paymaster to Replace Gas Fee 
-
-"GaslessPaymaster" was successfully deployed:
- - Contract address: 0x71C8e4F11988A5f0b70Eb93BcCacb94B1D6D758C
- - Contract source: contracts/GaslessPaymaster.sol:GaslessPaymaster
- - Encoded constructor arguments: 0x
-
-
-
-
-### Atlas
-
-Visit Atlas to Deploy Contract: https://app.atlaszk.com/ide?m=4baWX7
-
-
-# bnbcraft
--- contracts/deploy/deploy.ts
+++ contracts/deploy/deploy.ts
@@ -1,10 +1,41 @@
-import { deployContract } from "./utils";
+import { ethers } from "hardhat";
+import { Contract, ContractFactory } from "ethers";
 
-// An example of a basic deploy script
-// It will deploy a Greeter contract to selected network
-// as well as verify it on Block Explorer if possible for the network
-export default async function () {
-  const contractArtifactName = "Greeter";
-  const constructorArguments = ["Hi there!"];
-  await deployContract(contractArtifactName, constructorArguments);
+async function main() {
+  console.log("Deploying ZeekCraft contract...");
+
+  // Get the ContractFactory and Signer
+  const ZeekCraft: ContractFactory = await ethers.getContractFactory("ZeekCraft");
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying with account:", deployer.address);
+
+  // Deploy the contract
+  const zeekCraft: Contract = await ZeekCraft.deploy();
+
+  // Wait for the contract to be deployed
+  await zeekCraft.deployed();
+
+  console.log("ZeekCraft deployed to:", zeekCraft.address);
+
+  // Verify the contract on Etherscan
+  if (process.env.ETHERSCAN_API_KEY) {
+    console.log("Verifying contract on Etherscan...");
+    await run("verify:verify", {
+      address: zeekCraft.address,
+      constructorArguments: [],
+    });
+    console.log("Contract verified on Etherscan");
+  } else {
+    console.log("Skipping Etherscan verification");
+  }
 }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
